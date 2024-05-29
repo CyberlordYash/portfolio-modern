552254ec1f51aec3c73b36003e0639fc34fe7dd0
Yash Sachan <yashsachan321@gmail.com>
2024-05-29 17:41:28 +0530

    first commit

---
+++ components/Skills.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { InfiniteMovingCards } from "./ui/infiniteMovingCards";
+import { testimonials } from "@/data";
+
+const Skills = () => {
+  return (
+    <div className=" py-20">
+      <h1 className=" heading">
+        My <span className=" text-purple">Skills</span>
+      </h1>
+      <div className=" flex flex-col items-center ">
+        <div className=" h-[50vh] md: h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden">
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="fast"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
