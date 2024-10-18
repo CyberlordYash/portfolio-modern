186e8ca879030a43ab426ed5739c39b3ad0c3e1a
cyberlordyash <yashsachan321@gmail.com>
2024-10-18 16:37:48 +0530

    added a loader

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import Loading from "@/components/Loading";
 import RecentProjects from "@/components/RecentProjects";
 import Skills from "@/components/Skills";
 import { FloatingNav } from "@/components/ui/FloatingNav";
@@ -12,26 +13,40 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import { TracingBeam } from "@/components/ui/tracingBeam";
 
 import { navItems } from "@/data";
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  // This useEffect will set isLoading to false after 2 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 200); // 2 seconds delay
+
+    // Cleanup the timer in case the component is unmounted
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <main className=" relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip font-Quicksand">
-      <div className=" max-w-7xl w-full">
-        <FloatingNav navItems={navItems} className=" font-Quicksand" />
-        <TracingBeam>
-          <Hero />
-
-          <Grid />
-          <Experience />
-          <RecentProjects />
-
-          <Skills />
-          <Certificates />
-
-          <Approach />
-
-          <Footer />
-        </TracingBeam>
-      </div>
+    <main className="relative bg-black flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip font-Quicksand min-h-screen">
+      {isLoading ? (
+        <div className="flex justify-center items-center h-full w-full">
+          <Loading /> {/* Center the Loading component */}
+        </div>
+      ) : (
+        <div className="max-w-7xl w-full">
+          <FloatingNav navItems={navItems} className="font-Quicksand" />
+          <TracingBeam>
+            <Hero />
+            <Grid />
+            <Experience />
+            <RecentProjects />
+            <Skills />
+            <Certificates />
+            <Approach />
+            <Footer />
+          </TracingBeam>
+        </div>
+      )}
     </main>
   );
 }
---
+++ components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div id="load">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loading;
