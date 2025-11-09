06a62e260fbe7d426ee3b3afd581ff0e2e5f938d
cyberlordyash <yashsachan321@gmail.com>
2025-11-09 12:22:00 +0530

    improved loader

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -32,11 +32,26 @@ const Loading = () => {
     <AnimatePresence>
       {show && (
         <div className="loader">
-          <div className="dot"></div>
-          <div className="dot"></div>
-          <div className="dot"></div>
-          <div className="dot"></div>
-          <div className="dot"></div>
+          <div
+            className="orbe"
+            style={{ "--index": 0 } as React.CSSProperties}
+          ></div>
+          <div
+            className="orbe"
+            style={{ "--index": 1 } as React.CSSProperties}
+          ></div>
+          <div
+            className="orbe"
+            style={{ "--index": 2 } as React.CSSProperties}
+          ></div>
+          <div
+            className="orbe"
+            style={{ "--index": 3 } as React.CSSProperties}
+          ></div>
+          <div
+            className="orbe"
+            style={{ "--index": 4 } as React.CSSProperties}
+          ></div>
         </div>
       )}
     </AnimatePresence>
